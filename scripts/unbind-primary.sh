dc283e2a680cdbe96dbcb9e5246286daa7168934
mike <mike@exophase.com>
2021-08-01 01:08:46 -0700

    Cleanup

--- scripts/unbind-primary.sh
+++ scripts/unbind-primary.sh
@@ -1,11 +1,2 @@
 # Stop display manager
 systemctl stop display-manager.service
-## Uncomment the following line if you use GDM
-killall gdm-x-session
-
-# unbind VTConsoles
-echo 0 > /sys/class/vtconsole/vtcon0/bind
-echo 0 > /sys/class/vtconsole/vtcon1/bind
-
-# unbind efi framebuffer
-echo efi-framebuffer.0 > /sys/bus/platform/drivers/efi-framebuffer/unbind
